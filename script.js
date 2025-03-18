a39268d4ec2ffe382feb2ba27bd07102c7cded1e
a <a>
2025-03-18 10:26:28 +0100

    v1

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-var data = 
-[]
+var aMonths = new Array('styczeń','luty','marzec','kwiecień','maj','czerwiec','lipiec','sierpień','wrzesień','październik','listopad','grudzien');
+var date    = new Date();
+
+Calendar();
+
+document.querySelector('#prev').addEventListener('click',PrevMonth);
+document.querySelector('#next').addEventListener('click',NextMonth);
+
+function PrevMonth(){
+date = new Date(date.getFullYear(), date.getMonth() - 1, 1);
+Calendar();
+}
+
+function NextMonth(){
+date = new Date(date.getFullYear(), date.getMonth() + 1, 1);
+Calendar();
+}
+
+function Calendar(){
+
+const   td       = document.querySelectorAll('#calendar tbody td');
+const   firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+const   lastDay  = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+const   day      = firstDay.getDay() ? firstDay.getDay()-1 : 6;
+
+document.querySelector('#calendar_top').innerHTML = aMonths[date.getMonth()] + ' ' + date.getFullYear();
+
+let dzien = 1;
+for (let i = 0; i < td.length; i++) {
+     td[i].innerHTML = (i>=day && dzien<=lastDay.getDate()) ? dzien++ : '';
+     if (i>=35) td[i].style.display =  (day+dzien-1<36) ?  'none' : '';
+     }
+
+}
