c676af643615d10d6b64a3e3656a51695d4a06a5
a <a>
2025-03-25 07:30:59 +0100

    v2

--- script.js
+++ script.js
@@ -2,13 +2,12 @@
 var data = [
     { rok: "2020", ilosc: 6.23 },
     { rok: "2021", ilosc: 7.62 },
-    { rok: "2022", ilosc: 8.78 },
+    { rok: "2022", ilosc: 7.99 },
     { rok: "2023", ilosc: 6.55 },
     { rok: "2024", ilosc: 5.35 }
 ];
 
 let chart = document.getElementById("chart");
-let footer = document.getElementById("footer");
 
 
 
@@ -19,6 +18,4 @@ data.forEach(item => {
     bar.innerText = item.ilosc;
     chart.appendChild(bar);
 
-    let label = document.createElement("p");
-    label.innerText = item.rok;
 });
