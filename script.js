56ac4abf3410208f7a655f9b28eec933d7e3083f
a <a>
2025-03-25 08:35:30 +0100

    zegar

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
-var data = 
-[]
+const hour = document.getElementById('hour')
+const minute = document.getElementById('minute')
+const second = document.getElementById('second')
+const day = document.getElementById('day')
+
+var monthName = [
+    'Styczeń',
+    'Luty',
+    'Marzec',
+    'Kwiecień',
+    'Maj',
+    'Czerwiec',
+    'Lipiec',
+    'Sierpień',
+    'Wrzesień',
+    'Październik',
+    'Listopad',
+    'Grudzień'
+]
+const clock = setInterval(function time(){
+    let today = new Date()
+    let d = today.getDate()
+    let m = today.getMonth()    
+    let y = today.getFullYear()
+    let h = today.getHours()
+    let min = today.getMinutes()
+    let sec = today.getSeconds()
+
+    day.innerHTML = `${d} ${monthName[m]} ${y}`
+    hour.textContent = h
+    minute.innerText = min
+    second.innerText = sec
+})
